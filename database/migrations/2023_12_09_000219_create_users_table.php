ce6c4709452506b6aac4729ba018085abc8e9bd7
Chahine-Chahine <chahinesquare@hotmail.com>
2023-12-09 03:40:11 +0200

    created migrations to database

---
+++ database/migrations/2023_12_09_000219_create_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('user_id');
+            $table->string('username');
+            $table->string('email');
+            $table->string('password'); 
+            $table->unsignedBigInteger('user_type_id');
+            $table->foreign('user_type_id')->references('user_type_id')->on('user_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ database/migrations/2023_12_09_005321_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('order_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('user_id')->on('users');
+            $table->unsignedBigInteger('transaction_id');
+            $table->foreign('transaction_id')->references('transaction_id')->on('transactions');
+            $table->unsignedBigInteger('cart_id');
+            $table->foreign('cart_id')->references('cart_id')->on('shoppingcarts');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
